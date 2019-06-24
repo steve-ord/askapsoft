e1cf4a3880f0ec5560ed5937e6951e5e5db3e2ff
Stephen Ord <steve.ord@gmail.com>
2019-06-24 12:49:14 +1000

    added the fix_paths script

---
+++ fix_paths.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash 
+
+h_files=`find . -name "*.h"`
+c_files=`find . -name "*.cc"`
+t_files=`find . -name "*.tcc"`
+
+all_files="$h_files $c_files $t_files"
+
+for file in $all_files
+do	
+  # fix namespaces
+#  sed -i '' 's|'\ casa\;'|'\ casacore\;'|g' $file
+#  sed -i '' 's|'casa\:'|'casacore\:'|g' $file
+
+  #include paths	
+#  sed -i '' 's|'include\ \<askap/'|'include\ \<askap/askap/'|g' $file
+#  sed -i '' 's|'include\ \"askap/'|'include\ \"askap/askap/'|g' $file
+#  sed -i '' 's|'include\ \<askapparallel/'|'include\ \<askap/askapparallel/'|g' $file
+#  sed -i '' 's|'include\ \"askapparallel/'|'include\ \"askap/askapparallel/'|g' $file
+
+#  sed -i '' 's|'include\ \<utils/'|'include\ \<askap/scimath/utils/'|g' $file
+#  sed -i '' 's|'include\ \"utils/'|'include\ \"askap/scimath/utils/'|g' $file
+#  sed -i '' 's|'include\ \<scimath/'|'include\ \<askap/scimath/'|g' $file
+#  sed -i '' 's|'include\ \"scimath/'|'include\ \"askap/scimath/'|g' $file
+
+#  sed -i '' 's|'include\ \<components/AskapComponentImager.h'|'include\ \<askap/components/AskapComponentImager.h'|g' $file
+#  sed -i '' 's|'include\ \"components/AskapComponentImager.h'|'include\ \"askap/components/AskapComponentImager.h'|g' $file
+
+  sed -i '' 's|'include\ \<components/ComponentModels/'|'include\ \<casarest/components/ComponentModels/'|g' $file
+  sed -i '' 's|'include\ \"components/ComponentModels/'|'include\ \"casarest/components/ComponentModels/'|g' $file
+done
